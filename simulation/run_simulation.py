f455d4a1c0e96108147723569421377fb9daa125
mahaveer27 <mahaveerhekad@gmail.com>
2026-04-10 15:53:23 +0700

    Added simulation runner module

---
+++ simulation/run_simulation.py
@@ -0,0 +1,8 @@
+print("Autonomous Navigation Simulation Started")
+
+print("Loading grid map...")
+print("Running A* path planning...")
+print("Path found successfully")
+
+path = [(0,0), (1,0), (2,1), (3,2), (4,4)]
+print("Generated Path:", path)
