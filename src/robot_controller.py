6386df76e902fc8cd3c164e1474f815f2643df49
mahaveer27 <mahaveerhekad@gmail.com>
2026-04-10 16:41:49 +0700

    Added robot controller module

---
+++ src/robot_controller.py
@@ -0,0 +1,7 @@
+path = [(0, 0), (1, 0), (2, 0), (2, 1), (3, 2), (4, 3), (4, 4)]
+
+print("Robot controller started")
+print("Robot will follow this path:")
+
+for step in path:
+    print("Moving to", step)
